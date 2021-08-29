ac4696bae38e763392f72730e8c3fadcbfe2e1e7
EC2 Default User <ec2-user@ip-172-31-33-160.us-east-2.compute.internal>
2021-08-29 20:51:47 +0000

    after task 3 is running

--- run_docker.sh
+++ run_docker.sh
@@ -4,10 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=projectdocker .
+docker build --tag=projdocker .
 # Step 2: 
 # List docker images
 docker image ls
 # Step 3: 
 # Run flask app
-docker run -it projectdocker bash 
+docker run -it projdocker bash 
