104fadb2779cd06dd276a4a42a4baf80c5388b8a
EC2 Default User <ec2-user@ip-172-31-33-160.us-east-2.compute.internal>
2021-08-30 02:04:30 +0000

    push all the result files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="78dadb74358b/projdocker"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
+kubectl run projectkubernetes --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward projectkubernetes 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="78dadb74358b/projdocker"
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=78dadb74358b --password=maidocker123456789 &&\
+    docker image tag projdocker $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
